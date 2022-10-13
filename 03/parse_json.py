70a933fbd9ee7b6e743903bd31ca4b7a1ec844c4
Гайнитдинов Батырхан Азаматович <ba_gaynitdinov@bnipi.rosneft.ru>
2022-10-13 16:44:55 +0500

    fixed

--- 03/parse_json.py
+++ 03/parse_json.py
@@ -1,14 +1,6 @@
 import json
-# json_str = """{
-#     "name": "jane doe",
-#     "salary": "9000",
-#     "skills": "Raspberry_pi  Machine_Learning Web_Development",
-#     "email": "JaneDoe@pynative.com",
-#     "projects": "Python Data Mining, Python Data Science"
-# }
-# """
+
 def keyword_callback(word):
-    # print(word.upper())
     return word.upper()
 
 def parse_json(json_str: str, keyword_callback, required_fields=None, keywords=None):
@@ -19,20 +11,15 @@ def parse_json(json_str: str, keyword_callback, required_fields=None, keywords=N
             if keywords:
                 for word in keywords:
                     if word in data[field].split(' '):
-                        # print(keyword_callback(word))
                         final = keyword_callback(word)
                         output.append(final)
                     else:
                         line = f'no keyword "{word}" for field "{field}"'
                         output.append(line)
-                        # return f'no keyword "{word}" for field "{field}"'
             else:
-                # print('no keywords were provided')
                 return 'no keywords were provided'
     else:
-        # print('no required fields were provided')
         return 'no required fields were provided'
     return output
 
-# parse_json(json_str, keyword_callback, ['skills', 'email'], ['Machine_Learning'])
 
